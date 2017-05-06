bca8b190e33828cca7923395f8397ba3e91a1147
Arthur Kaul <arthur,kaul@gmx.de>
2017-05-06 23:37:00 +0200

    Test

---
+++ ArchRefServer/ArchRef_Server/src/main/java/de/arthurkaul/archref/exceptions/NodeTypeAlreadyExistException.java
@@ -0,0 +1,15 @@
+package de.arthurkaul.archref.exceptions;
+
+public class NodeTypeAlreadyExistException extends NodeTypeException {
+
+	public NodeTypeAlreadyExistException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public NodeTypeAlreadyExistException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
